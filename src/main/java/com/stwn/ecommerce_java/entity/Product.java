ca22cea953249a63685f09b7e99186ddc50c18e7
setyawandwiki <setyawandwiki1@gmail.com>
2025-02-04 12:35:11 +0700

    adding product, category, product category entity

---
+++ src/main/java/com/stwn/ecommerce_java/entity/Product.java
@@ -0,0 +1,39 @@
+package com.stwn.ecommerce_java.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "product")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    @Column(nullable = false)
+    private BigDecimal price;
+    @Column(name = "stock_quantity", nullable = false)
+    private Integer stockQuantity;
+    @Column(name = "weight", nullable = false)
+    private BigDecimal weight;
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime created_at;
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updated_at;
+}
